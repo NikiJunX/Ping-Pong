08bc73131c438765124efb9567f3d415f947f48b
NikiJunX <172074271+NikiJunX@users.noreply.github.com>
2025-05-04 13:09:41 +0300

    Update Ping Pong.py

--- Ping Pong.py
+++ Ping Pong.py
@@ -19,7 +19,7 @@ class Player(GameSprite):
     def __init__(self, player_speed, player_image, player_x, player_y):
         super().__init__(player_speed, player_image, player_x, player_y)
         self.speed = player_speed
-        self.image = transform.scale(image.load(player_image),(200,170))
+        self.image = transform.scale(image.load(player_image),(10,100))
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
@@ -39,23 +39,38 @@ class Ball(GameSprite):
     def __init__(self, player_speed, player_image, player_x, player_y):
         super().__init__(player_speed, player_image, player_x, player_y)
         self.speed = player_speed
-        self.image = transform.scale(image.load(player_image),(90,80))
+        self.image = transform.scale(image.load(player_image),(50,40))
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
 
-gs = Player(5, 'палка.png', 20, 200)
-gs1 = Player(5, 'палка.png', 500, 200)
+gs = Player(5, 'палка.png', 50, 160)
+gs1 = Player(5, 'палка.png', 650, 160)
 
 ball = Ball(5, 'мяч.png', 300, 200)
 
 clock = time.Clock()
 fps = 60
 
+speed_x = 3
+speed_y = 3
 
+
+finish = False
 game = True
 while game: 
     window.fill((41, 182, 214))
+
+    if finish != True:
+        ball.rect.x += speed_x
+        ball.rect.x += speed_y
+
+    if sprite.collide_rect(gs, ball) or sprite.collide_rect(gs1, ball):
+        speed_x *= -1
+
+    if ball.rect.y > 190 or ball.rect.y < 0:
+        speed_y *= -1
+
 
     for e in event.get():
         if e.type == QUIT:
